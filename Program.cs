0cdc8ebe794850497d4ee3b75f5fff369fb1c676
lg1033338 <lg1033338@otc.edu>
2025-11-11 21:36:27 -0600

    First Step In Game
    First Upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Final_Project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool gameOver = true;
+        
+        
+        while (gameOver)
+        {
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("Welcome to the Lost Doors");
+            Console.WriteLine("How do you want to start?");
+            Console.WriteLine("1) New Game");
+            Console.WriteLine("2) Continue");
+            Console.WriteLine("3) Exit");
+            Console.Write(">>> ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---------------------------------------------------");
+
+            switch (start)
+            {
+                case 1:
+
+                continue;
+                
+                case 2:
+                
+                continue;
+
+                case 3:
+                Console.WriteLine("You have exited!");
+                break;
+            }
+
+        } 
+    }
+}
